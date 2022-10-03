47594547420a9d79624438013a89dbeb5bcf7ba8
TiagoMontes <tiagosantos6303@gmail.com>
2022-10-03 17:07:17 -0500

    arrow functions

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,5 @@
+// 1- Variáveis
+
 // Var
 var x = 10;
 var y = 15;
@@ -39,4 +41,56 @@ logName();
 
 console.log(name);
 
-name = "Maria"; //TypeError: Assignment to constant variable.
+// name = "Maria"; //TypeError: Assignment to constant variable.
+
+
+//2 - Arrow Functions
+const sum = function sum(a, b) {
+    return a + b;
+}
+
+// const arrowSum = (a,b) => {
+//     return a + b
+// };
+const arrowSum = (a,b) => a + b;
+//Caso eu tenha apenas uma linha de função, posso tirar o return e o corpo da função {}
+
+console.log(sum(5,5));
+console.log(arrowSum(10,16));
+
+//Corpo completo
+const greeting = (name) => {
+    if(name) {
+        return `Hello ${name}`;
+    } else {
+        return "Bye!";
+    }
+}
+
+console.log(greeting("Tiago"));
+console.log(greeting());
+
+//Caso não precise de parâmetro
+const greeting2 = () => console.log("Hello World!");
+greeting2();
+
+//This no arrow Function
+const user = {
+    name: "Tiago",
+    // sayUserName() {
+    //     var self = this;
+    //     setTimeout(function() {
+    //         console.log(self);
+    //         console.log("Usuário: " + self.name);
+    //     },500);
+    // },
+    sayUserNameArrow() {
+        setTimeout(() => {
+            console.log(this);
+            console.log("Usuário: " + this.name);
+        }, 700)
+    }
+}
+
+// user.sayUserName();
+user.sayUserNameArrow();
