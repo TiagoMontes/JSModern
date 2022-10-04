a3241cef4b9eb6211559a90f914075e1eb102930
TiagoMontes <tiagosantos6303@gmail.com>
2022-10-03 19:20:18 -0500

    filter

--- javascript/script.js
+++ javascript/script.js
@@ -1,96 +1,126 @@
 // 1- Variáveis
 
-// Var
-var x = 10;
-var y = 15;
+    // Var
+    var x = 10;
+    var y = 15;
 
-if(y > 10){
-    var x = 5;
+    if(y > 10){
+        var x = 5;
+        console.log(x);
+    }
+
     console.log(x);
-}
 
-console.log(x);
+    //Let
+    let a = 10;
+    let b = 15;
 
-//Let
-let a = 10;
-let b = 15;
+    if(b > 10){
+        let a = 5;
+        console.log(a);
+    }
+    console.log(a)
 
-if(b > 10){
-    let a = 5;
-    console.log(a);
-}
-console.log(a)
+    let i = 100;
 
-let i = 100;
+    for (let i = 0; i < 5; i++) {
+        console.log(i);
+    }
 
-for (let i = 0; i < 5; i++) {
-    console.log(i);
-}
+    console.log(i)
 
-console.log(i)
+    // Const
+    function logName() {
+        const name = "Tiago";
+        console.log(name);
+    }
 
-// Const
-function logName() {
-    const name = "Tiago";
+    const name = "José";
+
+    logName();
+
     console.log(name);
-}
 
-const name = "José";
-
-logName();
-
-console.log(name);
-
-// name = "Maria"; //TypeError: Assignment to constant variable.
+    // name = "Maria"; //TypeError: Assignment to constant variable.
 
 
 //2 - Arrow Functions
-const sum = function sum(a, b) {
-    return a + b;
-}
-
-// const arrowSum = (a,b) => {
-//     return a + b
-// };
-const arrowSum = (a,b) => a + b;
-//Caso eu tenha apenas uma linha de função, posso tirar o return e o corpo da função {}
-
-console.log(sum(5,5));
-console.log(arrowSum(10,16));
-
-//Corpo completo
-const greeting = (name) => {
-    if(name) {
-        return `Hello ${name}`;
-    } else {
-        return "Bye!";
+    const sum = function sum(a, b) {
+        return a + b;
     }
-}
 
-console.log(greeting("Tiago"));
-console.log(greeting());
+    // const arrowSum = (a,b) => {
+    //     return a + b
+    // };
+    const arrowSum = (a,b) => a + b;
+    //Caso eu tenha apenas uma linha de função, posso tirar o return e o corpo da função {}
 
-//Caso não precise de parâmetro
-const greeting2 = () => console.log("Hello World!");
-greeting2();
+    console.log(sum(5,5));
+    console.log(arrowSum(10,16));
 
-//This no arrow Function
-const user = {
-    name: "Tiago",
-    // sayUserName() {
-    //     var self = this;
-    //     setTimeout(function() {
-    //         console.log(self);
-    //         console.log("Usuário: " + self.name);
-    //     },500);
-    // },
-    sayUserNameArrow() {
-        setTimeout(() => {
-            console.log(this);
-            console.log("Usuário: " + this.name);
-        }, 700)
+    //Corpo completo
+    const greeting = (name) => {
+        if(name) {
+            return `Hello ${name}`;
+        } else {
+            return "Bye!";
+        }
     }
-}
 
-// user.sayUserName();
-user.sayUserNameArrow();
+    console.log(greeting("Tiago"));
+    console.log(greeting());
+
+    //Caso não precise de parâmetro
+    const greeting2 = () => console.log("Hello World!");
+    greeting2();
+
+    //This no arrow Function
+    const user = {
+        name: "Tiago",
+        // sayUserName() {
+        //     var self = this;
+        //     setTimeout(function() {
+        //         console.log(self);
+        //         console.log("Usuário: " + self.name);
+        //     },500);
+        // },
+        sayUserNameArrow() {
+            setTimeout(() => {
+                console.log(this);
+                console.log("Usuário: " + this.name);
+            }, 700)
+        }
+    }
+
+    // user.sayUserName();
+    user.sayUserNameArrow();
+
+
+// 3- Filter
+    const arr = [1, 2, 3, 4, 5];
+
+    const highNumbers = arr.filter((n) => {
+        if (n >= 3) {
+            return n;
+        }
+    })
+
+    console.log(highNumbers);
+
+    //ex 2
+    const users = [
+        {name: "Tiago", available: true},
+        {name: "João", available: false},
+        {name: "Victor", available: false},
+        {name: "Caio", available: true},
+    ]
+
+    const availableUsers = users.filter((user) =>{
+        if (user.available === true) {
+            return console.log("Ele está online");
+        } else {
+            return console.log("Ele está offline");
+        }
+    })
+
+    console.log(availableUsers);
